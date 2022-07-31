b3903dad51c5567a1306c3e877018ee9b62295a4
Kamrul-Haque <utchas903@gmail.com>
2022-07-31 22:30:52 +0600

    Factory & Seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word(),
+            'user_id' => $this->faker->numberBetween(1, 100),
+            'description' => $this->faker->text(),
+        ];
+    }
+}
